59390a8073153d8921dca5a049511aca635069a3
htregnagoCNAM <htregnago@cfai-formation.fr>
2023-12-05 21:52:43 +0100

    Push TP5

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { ApiService } from './api.service';
+import { Observable } from 'rxjs';
+import { Produit } from './models/produit';
+import { MatDialog } from '@angular/material/dialog';
+import { HelloResponseModalComponent } from './hello-response-modal/hello-response-modal.component';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +12,40 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'tp05_tregnago_hugo';
+  nameHello = '';
+  login: string = '';
+  password: string = '';
+
+  nom: string = '';
+  prenom: string = '';
+  cnx: boolean = false;
+  produits$: Observable<Array<Produit>>;
+  constructor(private apiService: ApiService, private dialog: MatDialog) {
+    this.produits$ = this.apiService.getCatalogue();
+  }
+  connexion() {
+    this.apiService.loginClient(this.login, this.password).subscribe((c) => {
+      this.nom = c.nom;
+      this.prenom = c.prenom;
+      this.cnx = true;
+    });
+  }
+  hello(): void {
+    if (this.nameHello.trim() !== '') {
+      this.apiService.getHello(this.nameHello)
+        .subscribe(
+          (data) => {
+            const dialogRef = this.dialog.open(HelloResponseModalComponent, {
+              width: '400px',
+              data: data.nom  // Utilisez data.nom pour obtenir le nom depuis la réponse
+            });
+          },
+          (error) => {
+            console.error('Erreur lors de l\'appel à l\'API Hello :', error);
+          }
+        );
+    } else {
+      console.log('Veuillez saisir un nom pour Hello.');
+    }
+  }
 }
---
+++ src/environments/environment.ts
@@ -0,0 +1,7 @@
+export const environment = {
+    production: false,
+    backendHello: 'https://cnamapprenderhugo.onrender.com/api/hello',
+    backendLoginClient: 'https://cnamapprenderhugo.onrender.com/api/utilisateur/login',
+    backendCatalogue: 'https://cnamapprenderhugo.onrender.com/api/catalogue',
+  };
+  
